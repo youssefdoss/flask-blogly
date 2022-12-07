d6a7182e745fb48b3d9331632359cd42354c2291
Youssef Doss <youssefdoss97@gmail.com>
2022-12-07 16:28:48 -0500

    built initial setup

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -9,4 +9,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = 'SECRET!'
+debug = DebugToolbarExtension(app)
+
+# db.create_all()
+
+@app.get('/')
+def list_users():
+    '''List users and show home page with add button'''
+
+    users = User.query.all()
+    return render_template('list.html', users=users)
--- models.py
+++ models.py
@@ -1,1 +1,27 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    '''Connect to database'''
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    '''User class'''
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+        primary_key=True,
+        autoincrement=True)
+    first_name = db.Column(db.Text,
+        nullable=False)
+    last_name = db.Column(db.Text,
+        nullable=False)
+    image_url = db.Column(db.Text) # maybe add a default image url and make it not null
